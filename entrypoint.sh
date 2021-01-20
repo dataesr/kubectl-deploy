a0e52052273202f81b68606887da214b0c90b8eb
folland87 <frederic@olland.me>
2021-01-20 13:38:41 +0100

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,9 @@
-#!/bin/sh
+#!/bin/sh -l
 
 set -e
-
+echo "Decode kube config file"
 echo "$KUBE_CONFIG" | base64 -d > /tmp/kubeconfig
+echo "export kubeconfig"
 export KUBECONFIG=/tmp/kubeconfig
 echo "Restarting deployment/$1"
 exec /usr/bin/kubectl rollout restart deployment/$1
